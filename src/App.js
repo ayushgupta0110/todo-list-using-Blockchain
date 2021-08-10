b63b19d1aed25a2f1613767184295a8b132f40f9
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2021-08-10 23:46:03 +0530

    initial account setup and display

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import Web3 from 'web3';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 function App() {
+  const [account,setAccount] = useState("");
+
+const loadBlockchainData = async() => {
+  const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  const network = await web3.eth.net.getNetworkType();
+  console.log('network:',network);
+  //fetch the account information
+  const accountInfo = await web3.eth.getAccounts();
+  console.log('accountInfo:',accountInfo);
+  //set the account
+  setAccount(accountInfo[0]);
+}
+
+useEffect(() => {
+  loadBlockchainData();
+}, []);
+
   return (
     <div className="container">
       <h1>Hello,World!</h1>
+      <p>Your account: {account}</p>
     </div>
   );
 }
